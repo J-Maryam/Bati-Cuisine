89c2f6e1746c42bb41b076457c5b244b31434f9e
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-17 23:33:49 +0100

    modification dand la class Composant, rendre les attributs protected

--- src/models/entities/Composant.java
+++ src/models/entities/Composant.java
@@ -5,11 +5,11 @@ import models.enums.TypeComposant;
 import java.util.UUID;
 
 public class Composant {
-    private UUID id;
-    private String nom;
-    private float tauxTVA;
-    private TypeComposant typeComposant;
-    private Projet projet;
+    protected UUID id;
+    protected String nom;
+    protected float tauxTVA;
+    protected TypeComposant typeComposant;
+    protected Projet projet;
 
     public Composant() {}
     public Composant(UUID id, String nom, float tauxTVA,TypeComposant typeComposant, Projet projet) {
@@ -20,43 +20,4 @@ public class Composant {
         this.projet = projet;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public float getTauxTVA() {
-        return tauxTVA;
-    }
-
-    public void setTauxTVA(float tauxTVA) {
-        this.tauxTVA = tauxTVA;
-    }
-
-    public TypeComposant getTypeComposant() {
-        return typeComposant;
-    }
-
-    public void setTypeComposant(TypeComposant typeComposant) {
-        this.typeComposant = typeComposant;
-    }
-
-    public Projet getProjet() {
-        return projet;
-    }
-
-    public void setProjet(Projet projet) {
-        this.projet = projet;
-    }
 }
