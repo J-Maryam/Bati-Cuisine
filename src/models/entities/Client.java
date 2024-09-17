cb4b469f3b0204c85f46ed07ae9ea314262aa5df
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-17 23:00:59 +0100

    creation de la classe Client

--- src/models/entities/Client.java
+++ src/models/entities/Client.java
@@ -1,4 +1,60 @@
 package models.entities;
 
+import java.util.UUID;
+
 public class Client {
+    private UUID id;
+    private String nom;
+    private String adresse;
+    private String telephone;
+    private boolean isProfessional;
+
+    public Client(){}
+    public Client(UUID id, String nom, String adresse, String telephone, boolean isProfessional) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.telephone = telephone;
+        this.isProfessional = isProfessional;
+    }
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public boolean isProfessional() {
+        return isProfessional;
+    }
+
+    public void setProfessional(boolean isProfessional) {
+        this.isProfessional = isProfessional;
+    }
+
 }
