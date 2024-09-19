67e25b20e6471428e02b00f6e940a1fc0436b731
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-19 16:50:34 +0100

    ajout de getters et setters dans la classe de Composant

--- src/models/entities/Composant.java
+++ src/models/entities/Composant.java
@@ -20,5 +20,45 @@ public abstract class Composant {
         this.projet = projet;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public float getTauxTVA() {
+        return tauxTVA;
+    }
+
+    public void setTauxTVA(float tauxTVA) {
+        this.tauxTVA = tauxTVA;
+    }
+
+    public TypeComposant getTypeComposant() {
+        return typeComposant;
+    }
+
+    public void setTypeComposant(TypeComposant typeComposant) {
+        this.typeComposant = typeComposant;
+    }
+
+    public Projet getProjet() {
+        return projet;
+    }
+
+    public void setProjet(Projet projet) {
+        this.projet = projet;
+    }
+
     public abstract double calculerCout();
 }
