72cc0592cc06d298f7755d3b79dffeba217911fe
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-23 10:08:22 +0100

    creation de devi

--- src/services/impls/DeviServiceImpl.java
+++ src/services/impls/DeviServiceImpl.java
@@ -1,13 +1,18 @@
 package services.impls;
 
 import models.entities.Devi;
+import repository.DeviRepository;
 import services.DeviService;
 
 import java.util.UUID;
 
 public class DeviServiceImpl implements DeviService {
+    private DeviRepository deviRepository;
+    public DeviServiceImpl(DeviRepository deviRepository) {
+        this.deviRepository = deviRepository;
+    }
     @Override
     public UUID addDevi(Devi devi) {
-        return null;
+        return deviRepository.addDevi(devi);
     }
 }
---
+++ src/ui/DeviMenu.java
@@ -0,0 +1,73 @@
+package ui;
+
+import models.entities.Devi;
+import models.entities.Projet;
+import services.DeviService;
+import services.ProjetService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class DeviMenu {
+    private Scanner scanner = new Scanner(System.in);
+    private DeviService deviService;
+    private ProjetService projetService;
+
+    public DeviMenu(DeviService deviService, ProjetService projetService) {
+        this.deviService = deviService;
+        this.projetService = projetService;
+    }
+
+    public void addDevi(UUID projetId) {
+        System.out.println("--- Enregistrement du Devis ---");
+
+        System.out.print("Entrez la date d'émission du devis (format : jj/mm/aaaa) : ");
+        String dateEmissionStr = scanner.nextLine();
+        LocalDate dateEmission = LocalDate.parse(dateEmissionStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        System.out.print("Entrez la date de validité du devis (format : jj/mm/aaaa) : ");
+        String dateValiditeStr = scanner.nextLine();
+        LocalDate dateValidite = LocalDate.parse(dateValiditeStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        float montantEstime = (float) projetService.calculateCoutTotalFinal(projetId, new Projet());
+
+        Optional<Projet> projet = projetService.getProjetById(projetId);
+
+        if (projet.isEmpty()) {
+            System.out.println("Projet non trouvé. Impossible de créer le devis.");
+            return;
+        }
+
+        Devi devi = new Devi();
+        devi.setDateEmission(dateEmission);
+        devi.setDateValidite(dateValidite);
+        devi.setMontantEstime(montantEstime);
+        devi.setProjet(projet.get());
+
+        while (true){
+            System.out.print("Souhaitez-vous enregistrer le devis ? (y/n) : ");
+            String reponse = scanner.nextLine();
+
+            if (reponse.equalsIgnoreCase("y")) {
+                devi.setAccepte(true);
+                UUID deviId = deviService.addDevi(devi);
+                if (deviId != null) {
+                    System.out.println("Devis enregistré avec succès !");
+                } else {
+                    System.out.println("Erreur lors de l'enregistrement du devis.");
+                }
+                break;
+            } else if (reponse.equalsIgnoreCase("n")) {
+                devi.setAccepte(false);
+                deviService.addDevi(devi);
+                break;
+            }else{
+                System.out.println("Réponse invalide. Veuillez répondre par 'y' ou 'n'.");
+            }
+        }
+    }
+
+}
